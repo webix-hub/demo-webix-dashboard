d520fb92225d27a38dc0e6823ebf50d8b973911c
Nastya <akovalenko@xbsoftware.com>
2018-03-13 20:10:27 +0300

    [add] step-1

---
+++ data/data.js
@@ -0,0 +1,15 @@
+var data = [
+	{ id:1, country:'Germany', code: "DE", sales:357, visitors:83 },
+	{ id:2, country:'United States', code: "US", sales:9834, visitors:323 },
+	{ id:3, country:'Brazil', code: "BR", sales:8516, visitors:207 },
+	{ id:4, country:'Canada', code: "CA", sales:9985, visitors:36 },
+	{ id:5, country:'France', code: "FR", sales:644, visitors:67 },
+	{ id:6, country:'Russia', code: "RU", sales:17100,visitors:144 },
+	{ id:7, country:'China', code: "CN", sales:9597, visitors:137 },
+	{ id:8, country:'Australia', code: "AU", sales:7692, visitors:24 },
+	{ id:9, country:'Algeria', code: "DZ", sales:2382, visitors:41 },
+	{ id:10, country:'South Africa'	, code: "ZA", sales:1220, visitors:56 },
+	{ id:11, country:'Sudan', code: "SD", sales:1886, visitors:40 },
+	{ id:12, country:'India', code: "IN", sales:3287, visitors:132 },
+	{ id:13, country:'Belarus', code: "BY", sales:208, visitors:10  }
+];
---
+++ helpers/themes.js
@@ -0,0 +1,24 @@
+function setTheme(name){
+	var links = document.getElementsByTagName("link");
+		
+	for (var i=0; i<links.length; i++){
+
+		var lname = links[i].getAttribute("title");
+
+		if (lname){
+
+			if (lname === name){
+				links[i].disabled = false;
+			} else {
+				links[i].disabled = true;
+			}
+		} 
+	}
+	//remove old css
+	var theme = webix.storage.local.get("theme");
+	if (theme)
+		webix.html.removeCss(document.body, "theme-"+theme);
+	//add new css
+	webix.html.addCss(document.body, "theme-"+name);
+	webix.storage.local.put("theme", name);
+};
